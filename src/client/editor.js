cdb61dc7480a64ad01b4b23fcf4574c6ddf25019
Guillermo <gui@mit.edu>
2017-02-10 17:36:20 -0500

    reset?

--- src/client/editor.js
+++ src/client/editor.js
@@ -63,10 +63,6 @@ export default class Editor extends Component {
         let initial_val = (location.search != "?reset" && localStorage.raw_grammar) 
             || require('./arithmetic.ne')
 
-        console.log(location.search != "?reset", initial_val)
-
-        initial_val = require('./arithmetic.ne')
-
         this.compile(initial_val);
         var cm = CodeMirror(this.refs.wrap, {
             mode: 'nearley',
