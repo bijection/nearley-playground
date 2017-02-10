ad7d0c405d85e43d0e4063e3ce2532e2b8a9de6e
Guillermo <gui@mit.edu>
2017-02-10 15:22:31 -0500

    add immature emoji stuff

--- src/client/header.js
+++ src/client/header.js
@@ -12,14 +12,14 @@ export default class Header extends Component{
                 className={this.state.hidden ? 'show' : 'hide'}
                 >
                 {this.state.hidden 
-                    ? <div><b>Nearley Parser Playground</b> <span className='grey'>(click to show header)</span></div>
+                    ? <div><b>⛹ Nearley Parser Playground</b> <span className='grey'>(click to show header)</span></div>
                     : '[hide header]'}
             </div>
             {this.state.hidden
                 ? ''
                 : <div>
                     <div className='title-wrap'>
-                        <span className='title'>Nearley Parser Playground</span>
+                        <span className='title'>⛹ Nearley Parser Playground</span>
                     </div>
                     <div>Experiment with <a href='https://hardmath123.github.io/'>Hardmath123</a>'s incredible <a href="http://nearley.js.org/">Nearley Parser</a>!
                         Nearley will parse anything you throw at it (including ambiguous grammars!) without complaining or going into a <s>sulk</s> infinite loop.
