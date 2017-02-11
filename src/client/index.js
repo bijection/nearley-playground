a1b72eba9e2009b3f411a2639c468241c51b23a0
Guillermo <gui@mit.edu>
2017-02-11 16:05:31 -0500

    add prefilled comtent

--- src/client/index.js
+++ src/client/index.js
@@ -1,24 +1,5 @@
 import ReactDOM from 'react-dom'
 import React, {Component} from 'react'
-import Header from './header'
-import Editor from './editor'
-import Tester from './tester'
-
-import "./main.css"
-
-class Playground extends Component {
-    state={
-        grammar: undefined
-    };
-    render(){
-        return <div className='playground'>
-            <Header />
-            <div className='playground-body'>
-                <Editor setGrammar={grammar=>this.setState({grammar})}/>
-                <Tester grammar={this.state.grammar}/>
-            </div>
-        </div>
-    }
-}
+import Playground from './playground'
 
 ReactDOM.render(<Playground/>, document.getElementById('mount'))
