7d7eb9d80900039583d1e778d305921ebbd2dcd2
Guillermo <gui@mit.edu>
2019-02-05 12:44:46 -0800

    update compile function to expose moo

--- src/client/compile.js
+++ src/client/compile.js
@@ -1,205 +1,232 @@
 import nearley from 'nearley'
 import bootstraped from 'nearley/lib/nearley-language-bootstrapped'
 export default function Compile(structure, opts) {
-    var unique = uniquer();
+    var unique = uniquer()
     if (!opts.alreadycompiled) {
-        opts.alreadycompiled = [];
+        opts.alreadycompiled = []
     }
 
     var result = {
         rules: [],
         body: [], // @directives list
         config: {}, // @config value
+        customTokens: [], // %tokens
         macros: {},
         start: ''
-    };
+    }
 
     for (var i = 0; i < structure.length; i++) {
-        var productionRule = structure[i];
-        markRange(productionRule.name, productionRule.pos, productionRule.name && productionRule.name.length)
+        var productionRule = structure[i]
+        markRange(
+            productionRule.name,
+            productionRule.pos,
+            productionRule.name && productionRule.name.length
+        )
 
         if (productionRule.body) {
             // This isn't a rule, it's an @directive.
             if (!opts.nojs) {
-                result.body.push(productionRule.body);
+                result.body.push(productionRule.body)
             }
         } else if (productionRule.include) {
             // Include file
-            var path;
+            var path
             if (!productionRule.builtin) {
                 path = require('path').resolve(
-                    opts.file ? require('path').dirname(opts.file) : process.cwd(),
+                    opts.file
+                        ? require('path').dirname(opts.file)
+                        : process.cwd(),
                     productionRule.include
-                );
+                )
             } else {
                 path = productionRule.include
             }
             if (opts.alreadycompiled.indexOf(path) === -1) {
-                opts.alreadycompiled.push(path);
-                if(path === "postprocessors.ne"){
+                opts.alreadycompiled.push(path)
+                if (path === 'postprocessors.ne') {
                     var f = require('nearley/builtin/postprocessors.ne')
-                }else if(path === "whitespace.ne"){
+                } else if (path === 'whitespace.ne') {
                     var f = require('nearley/builtin/whitespace.ne')
-                }else if(path === "string.ne"){
+                } else if (path === 'string.ne') {
                     var f = require('nearley/builtin/string.ne')
-                }else if(path === "number.ne"){
+                } else if (path === 'number.ne') {
                     var f = require('nearley/builtin/number.ne')
-                }else if(path === "cow.ne"){
+                } else if (path === 'cow.ne') {
                     var f = require('nearley/builtin/cow.ne')
                 }
 
-                var parserGrammar = nearley.Grammar.fromCompiled(bootstraped);
-                var parser = new nearley.Parser(parserGrammar);
-                parser.feed(f);
-                var c = Compile(parser.results[0], {file: path, __proto__:opts});
+                var parserGrammar = nearley.Grammar.fromCompiled(bootstraped)
+                var parser = new nearley.Parser(parserGrammar)
+                parser.feed(f)
+                var c = Compile(parser.results[0], {
+                    file: path,
+                    __proto__: opts
+                })
 
-                result.rules = result.rules.concat(c.rules);
-                result.body  = result.body.concat(c.body);
+                result.rules = result.rules.concat(c.rules)
+                result.body = result.body.concat(c.body)
                 // result.customTokens = result.customTokens.concat(c.customTokens);
                 Object.keys(c.config).forEach(function(k) {
-                    result.config[k] = c.config[k];
-                });
+                    result.config[k] = c.config[k]
+                })
                 Object.keys(c.macros).forEach(function(k) {
-                    result.macros[k] = c.macros[k];
-                });
+                    result.macros[k] = c.macros[k]
+                })
             }
         } else if (productionRule.macro) {
             result.macros[productionRule.macro] = {
-                'args': productionRule.args,
-                'exprs': productionRule.exprs
-            };
+                args: productionRule.args,
+                exprs: productionRule.exprs
+            }
         } else if (productionRule.config) {
             // This isn't a rule, it's an @config.
             result.config[productionRule.config] = productionRule.value
         } else {
-            produceRules(productionRule.name, productionRule.rules, {});
+            produceRules(productionRule.name, productionRule.rules, {})
             if (!result.start) {
-                result.start = productionRule.name;
+                result.start = productionRule.name
             }
         }
     }
 
-    return result;
+    return result
 
-    function markRange(name, start, length){
+    function markRange(name, start, length) {
         // console.log(name, [start, start + length])
-        if(opts.rangeCallback){
+        if (opts.rangeCallback) {
             opts.rangeCallback(name, start, start + length)
         }
     }
 
     function produceRules(name, rules, env) {
         for (var i = 0; i < rules.length; i++) {
-            var rule = buildRule(name, rules[i], env);
+            var rule = buildRule(name, rules[i], env)
             if (opts.nojs) {
-                rule.postprocess = null;
+                rule.postprocess = null
             }
-            result.rules.push(rule);
+            result.rules.push(rule)
         }
     }
 
     function buildRule(ruleName, rule, env) {
-        var tokens = [];
+        var tokens = []
         for (var i = 0; i < rule.tokens.length; i++) {
-            var token = buildToken(ruleName, rule.tokens[i], env);
+            var token = buildToken(ruleName, rule.tokens[i], env)
             if (token !== null) {
-                tokens.push(token);
+                tokens.push(token)
             }
         }
-        return new nearley.Rule(
-            ruleName,
-            tokens,
-            rule.postprocess
-        );
+        return new nearley.Rule(ruleName, tokens, rule.postprocess)
     }
 
     function buildToken(ruleName, token, env) {
         if (typeof token === 'string') {
             if (token === 'null') {
-                return null;
+                return null
             }
-            return token;
+            return token
         }
 
         if (token instanceof RegExp) {
-            return token;
+            return token
         }
 
         if (token.literal) {
             if (!token.literal.length) {
-                return null;
+                return null
             }
-            if (token.literal.length === 1) {
-                return token;
+            if (token.literal.length === 1 || result.config.lexer) {
+                return token
             }
-            return buildStringToken(ruleName, token, env);
+            return buildStringToken(ruleName, token, env)
         }
         if (token.token) {
-            return token;
+            if (result.config.lexer) {
+                var name = token.token
+                if (result.customTokens.indexOf(name) === -1) {
+                    result.customTokens.push(name)
+                }
+                var expr =
+                    result.config.lexer +
+                    '.has(' +
+                    JSON.stringify(name) +
+                    ') ? {type: ' +
+                    JSON.stringify(name) +
+                    '} : ' +
+                    name
+                return { token: '(' + expr + ')' }
+            }
+
+            return token
         }
 
         if (token.subexpression) {
-            return buildSubExpressionToken(ruleName, token, env);
+            return buildSubExpressionToken(ruleName, token, env)
         }
 
         if (token.ebnf) {
-            return buildEBNFToken(ruleName, token, env);
+            return buildEBNFToken(ruleName, token, env)
         }
 
         if (token.macrocall) {
-            return buildMacroCallToken(ruleName, token, env);
+            return buildMacroCallToken(ruleName, token, env)
         }
 
         if (token.mixin) {
             if (env[token.mixin]) {
-                return buildToken(ruleName, env[token.mixin], env);
+                return buildToken(ruleName, env[token.mixin], env)
             } else {
-                throw new Error("Unbound variable: " + token.mixin);
+                throw new Error('Unbound variable: ' + token.mixin)
             }
         }
 
-        throw new Error("unrecognized token: " + JSON.stringify(token));
+        throw new Error('unrecognized token: ' + JSON.stringify(token))
     }
 
     function buildStringToken(ruleName, token, env) {
-
-        var newname = unique(ruleName + "$string");
+        var newname = unique(ruleName + '$string')
         markRange(newname, token.pos, JSON.stringify(token.literal).length)
 
-        produceRules(newname, [
-            {
-                tokens: token.literal.split("").map(function charLiteral(d) {
-                    return {
-                        literal: d
-                    };
-                }),
-                postprocess: {builtin: "joiner"}
-            }
-        ], env);
-        return newname;
+        produceRules(
+            newname,
+            [
+                {
+                    tokens: token.literal
+                        .split('')
+                        .map(function charLiteral(d) {
+                            return {
+                                literal: d
+                            }
+                        }),
+                    postprocess: { builtin: 'joiner' }
+                }
+            ],
+            env
+        )
+        return newname
     }
 
     function buildSubExpressionToken(ruleName, token, env) {
-        var data = token.subexpression;
-        var name = unique(ruleName + "$subexpression");
+        var data = token.subexpression
+        var name = unique(ruleName + '$subexpression')
         //structure.push({"name": name, "rules": data});
-        produceRules(name, data, env);
-        return name;
+        produceRules(name, data, env)
+        return name
     }
 
     function buildEBNFToken(ruleName, token, env) {
         switch (token.modifier) {
-            case ":+":
-                return buildEBNFPlus(ruleName, token, env);
-            case ":*":
-                return buildEBNFStar(ruleName, token, env);
-            case ":?":
-                return buildEBNFOpt(ruleName, token, env);
+            case ':+':
+                return buildEBNFPlus(ruleName, token, env)
+            case ':*':
+                return buildEBNFStar(ruleName, token, env)
+            case ':?':
+                return buildEBNFOpt(ruleName, token, env)
         }
     }
 
     function buildEBNFPlus(ruleName, token, env) {
-        var name = unique(ruleName + "$ebnf");
+        var name = unique(ruleName + '$ebnf')
         /*
         structure.push({
             name: name,
@@ -211,20 +238,24 @@ export default function Compile(structure, opts) {
             }]
         });
         */
-        produceRules(name,
-            [{
-                tokens: [token.ebnf],
-            }, {
-                tokens: [token.ebnf, name],
-                postprocess: {builtin: "arrconcat"}
-            }],
+        produceRules(
+            name,
+            [
+                {
+                    tokens: [token.ebnf]
+                },
+                {
+                    tokens: [token.ebnf, name],
+                    postprocess: { builtin: 'arrconcat' }
+                }
+            ],
             env
-        );
-        return name;
+        )
+        return name
     }
 
     function buildEBNFStar(ruleName, token, env) {
-        var name = unique(ruleName + "$ebnf");
+        var name = unique(ruleName + '$ebnf')
         /*
         structure.push({
             name: name,
@@ -236,20 +267,24 @@ export default function Compile(structure, opts) {
             }]
         });
         */
-        produceRules(name,
-            [{
-                tokens: [],
-            }, {
-                tokens: [token.ebnf, name],
-                postprocess: {builtin: "arrconcat"}
-            }],
+        produceRules(
+            name,
+            [
+                {
+                    tokens: []
+                },
+                {
+                    tokens: [token.ebnf, name],
+                    postprocess: { builtin: 'arrconcat' }
+                }
+            ],
             env
-        );
-        return name;
+        )
+        return name
     }
 
     function buildEBNFOpt(ruleName, token, env) {
-        var name = unique(ruleName + "$ebnf");
+        var name = unique(ruleName + '$ebnf')
         /*
         structure.push({
             name: name,
@@ -262,46 +297,50 @@ export default function Compile(structure, opts) {
             }]
         });
         */
-        produceRules(name,
-            [{
-                tokens: [token.ebnf],
-                postprocess: {builtin: "id"}
-            }, {
-                tokens: [],
-                postprocess: {builtin: "nuller"}
-            }],
+        produceRules(
+            name,
+            [
+                {
+                    tokens: [token.ebnf],
+                    postprocess: { builtin: 'id' }
+                },
+                {
+                    tokens: [],
+                    postprocess: { builtin: 'nuller' }
+                }
+            ],
             env
-        );
-        return name;
+        )
+        return name
     }
 
     function buildMacroCallToken(ruleName, token, env) {
-        var name = unique(ruleName + "$macrocall");
-        var macro = result.macros[token.macrocall];
+        var name = unique(ruleName + '$macrocall')
+        var macro = result.macros[token.macrocall]
         if (!macro) {
-            throw new Error("Unkown macro: "+token.macrocall);
+            throw new Error('Unkown macro: ' + token.macrocall)
         }
         if (macro.args.length !== token.args.length) {
-            throw new Error("Argument count mismatch.");
+            throw new Error('Argument count mismatch.')
         }
-        var newenv = {__proto__: env};
-        for (var i=0; i<macro.args.length; i++) {
-            var argrulename = unique(ruleName + "$macrocall");
-            newenv[macro.args[i]] = argrulename;
-            produceRules(argrulename, [token.args[i]], env);
+        var newenv = { __proto__: env }
+        for (var i = 0; i < macro.args.length; i++) {
+            var argrulename = unique(ruleName + '$macrocall')
+            newenv[macro.args[i]] = argrulename
+            produceRules(argrulename, [token.args[i]], env)
             //structure.push({"name": argrulename, "rules":[token.args[i]]});
             //buildRule(name, token.args[i], env);
         }
-        produceRules(name, macro.exprs, newenv);
-        return name;
+        produceRules(name, macro.exprs, newenv)
+        return name
     }
 }
 
 function uniquer() {
-    var uns = {};
-    return unique;
+    var uns = {}
+    return unique
     function unique(name) {
-        var un = uns[name] = (uns[name] || 0) + 1;
-        return name + '$' + un;
+        var un = (uns[name] = (uns[name] || 0) + 1)
+        return name + '$' + un
     }
 }
--- src/worker/index.js
+++ src/worker/index.js
@@ -1,21 +1,23 @@
 import nearley from 'nearley'
+import mooo from 'moo'
 
-function get_exports(source){
-    let module = {exports:''}
+function get_exports(source) {
+    let moo = mooo
+    let module = { exports: '' }
     eval(source)
     return module.exports
 }
 
-onmessage = function({data}){
-    let outputs = [];
+onmessage = function({ data }) {
+    let outputs = []
 
-    const {source, test} = data
+    const { source, test } = data
 
     try {
-        let parser = new nearley.Parser( get_exports(source) )
+        let parser = new nearley.Parser(get_exports(source))
         parser.feed(test)
         outputs = parser.results
-    } catch(e) {
+    } catch (e) {
         console.log(e)
     }
 
