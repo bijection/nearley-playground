71287c0835cfc1321873a319287504b0c2891fde
Guillermo <gui@mit.edu>
2021-02-10 18:28:36 -0800

    upgrade nearley, switch to new test generation logic

--- src/client/tester.js
+++ src/client/tester.js
@@ -1,74 +1,83 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import nearley from 'nearley'
+import gen from 'nearley/lib/unparse'
 import Inspector from 'react-inspector'
-import gen from './generate.js'
 import mooo from 'moo'
 
 export default class Tester extends Component {
     // state = {
     //     tests: ['1 + 1', 'ln(5 + sin(3 + 4*e))'],
     // };
-    setTest(i, test){
-        let {tests} = this.props
+    setTest(i, test) {
+        let { tests } = this.props
         let changed = tests.slice(0)
         changed[i] = test
         this.props.setTests(changed)
     }
-    deleteTest(i, shouldFocus=false){
-        let {tests} = this.props
+    deleteTest(i, shouldFocus = false) {
+        let { tests } = this.props
         let changed = tests.slice(0)
         changed.splice(i, 1)
         this.props.setTests(changed)
-        if(shouldFocus === true) setImmediate(e => {
-            this.refs['test'+Math.min(i,changed.length - 1)].focus()
-        })
+        if (shouldFocus === true)
+            setImmediate(e => {
+                this.refs['test' + Math.min(i, changed.length - 1)].focus()
+            })
     }
-    addTest(){
-        let {tests} = this.props
+    addTest() {
+        let { tests } = this.props
         this.props.setTests([...tests, ''])
         setImmediate(e => {
-            this.refs['test'+tests.length].focus()
+            this.refs['test' + tests.length].focus()
         })
     }
-    genTest(){
+    genTest() {
         let grammar = get_exports(this.props.grammar)
 
         if (grammar.Lexer && grammar.Lexer.re) {
-          grammar.ParserStart = grammar.Lexer.re;
+            grammar.ParserStart = grammar.Lexer.re
         }
 
-        let example = gen(grammar, grammar.ParserStart);
-        let {tests} = this.props
+        let example = gen(grammar, grammar.ParserStart, 10)
+        let { tests } = this.props
         this.props.setTests([...tests, example])
     }
-    render(){
-
+    render() {
         // let grammar = get_exports(this.props.grammar)
 
-        return <div className='tester' onKeyPress={e => {
-            if(e.key === 'Enter' && e.shiftKey) {
-                e.preventDefault()
-                this.addTest()
-            }
-        }}>
-            <div className='tests'>
-                {this.props.tests.map((t, i) =>
-                    <Test grammar={this.props.grammar}
-                        setErrors={this.props.setErrors}
-                        key={i}
-                        ref={'test'+i}
-                        setTest={this.setTest.bind(this,i)}
-                        deleteTest={this.deleteTest.bind(this,i)}
-                        test={t}/>
-                )}
+        return (
+            <div
+                className="tester"
+                onKeyPress={e => {
+                    if (e.key === 'Enter' && e.shiftKey) {
+                        e.preventDefault()
+                        this.addTest()
+                    }
+                }}
+            >
+                <div className="tests">
+                    {this.props.tests.map((t, i) => (
+                        <Test
+                            grammar={this.props.grammar}
+                            setErrors={this.props.setErrors}
+                            key={i}
+                            ref={'test' + i}
+                            setTest={this.setTest.bind(this, i)}
+                            deleteTest={this.deleteTest.bind(this, i)}
+                            test={t}
+                        />
+                    ))}
+                </div>
+                <div className="test-create">
+                    <div className="test-add" onClick={this.addTest.bind(this)}>
+                        + Add Test
+                    </div>
+                    <div className="test-generate" onClick={this.genTest.bind(this)}>
+                        Generate
+                    </div>
+                </div>
             </div>
-            <div className='test-create'>
-                <div className='test-add'
-                    onClick={this.addTest.bind(this)}>+ Add Test</div>
-                <div className='test-generate'
-                    onClick={this.genTest.bind(this)}>Generate</div>
-            </div>
-        </div>
+        )
     }
 }
 
@@ -80,23 +89,22 @@ function get_exports(source) {
 }
 
 class Test extends Component {
-    state={outputs: []}
+    state = { outputs: [] }
     worker = new Worker('./dist/worker.bundle.js')
-    focus(){
+    focus() {
         this.refs.input.focus()
     }
-    keyDown(e){
-        if(e.key === "Backspace" && e.target.value === '')
-            this.props.deleteTest(true)
+    keyDown(e) {
+        if (e.key === 'Backspace' && e.target.value === '') this.props.deleteTest(true)
     }
-    runTest(t){
+    runTest(t) {
         this.setState({
             testingTest: t,
             testingGrammar: this.props.grammar,
         })
 
         new Promise((res, rej) => {
-            this.worker.postMessage({test: t, source: this.props.grammar})
+            this.worker.postMessage({ test: t, source: this.props.grammar })
             this.worker.onmessage = e => {
                 res(e.data)
                 clearTimeout(this.timeout)
@@ -107,49 +115,54 @@ class Test extends Component {
                 rej('Possible infinite loop detected! Check your grammar for infinite recursion.')
             }, 5000)
         })
-        .then(outputs => this.setState({outputs}))
-        .catch(e => {
-            console.warn(e)
-            this.props.setErrors(e)
-            this.setState({outputs: []})
-        })
+            .then(outputs => this.setState({ outputs }))
+            .catch(e => {
+                console.warn(e)
+                this.props.setErrors(e)
+                this.setState({ outputs: [] })
+            })
     }
-    setTest(t){
+    setTest(t) {
         this.props.setTest(t)
         this.runTest(t)
     }
-    componentWillUnmount(){
+    componentWillUnmount() {
         clearTimeout(this.timeout)
         this.worker.terminate()
     }
-    render(){
-        let {outputs} = this.state;
-        if(this.state.testingTest != this.props.test
-            || this.state.testingGrammar != this.props.grammar) {
+    render() {
+        let { outputs } = this.state
+        if (
+            this.state.testingTest != this.props.test ||
+            this.state.testingGrammar != this.props.grammar
+        ) {
             this.runTest(this.props.test)
         }
 
         setImmediate(e => {
-            if(!this.refs.input) return
+            if (!this.refs.input) return
             this.refs.input.style.height = 0
             this.refs.input.style.height = this.refs.input.scrollHeight
         })
 
-        return <div className={'test ' + (outputs.length > 0 ? 'match' : '')}>
-            <div className='test-x' onClick={this.props.deleteTest}>
-                <div>{'\u00d7'}</div>
-            </div>
-            <textarea
-                ref='input'
-                placeholder="Type a test..."
-                onChange={e => this.setTest(e.target.value)}
-                value={this.props.test}
-                onKeyDown={this.keyDown.bind(this)}
+        return (
+            <div className={'test ' + (outputs.length > 0 ? 'match' : '')}>
+                <div className="test-x" onClick={this.props.deleteTest}>
+                    <div>{'\u00d7'}</div>
+                </div>
+                <textarea
+                    ref="input"
+                    placeholder="Type a test..."
+                    onChange={e => this.setTest(e.target.value)}
+                    value={this.props.test}
+                    onKeyDown={this.keyDown.bind(this)}
                 />
-            {outputs.map((output, i) =>
-                <div className='output' key={i}>
-                    <Inspector data={output}/>
-                </div>)}
-        </div>
+                {outputs.map((output, i) => (
+                    <div className="output" key={i}>
+                        <Inspector data={output} />
+                    </div>
+                ))}
+            </div>
+        )
     }
 }
